c09d36a0ed3ec153263145ff76cc909f32101c0f
SecurePi by Megahunt <158031960+SecurePi@users.noreply.github.com>
2024-09-24 13:18:28 +0800

    Update uart.py

--- uart.py
+++ uart.py
@@ -2,34 +2,19 @@ import serial
 import time
 import sys
 
-def serial_read(port, baud_rate, timeouts):
-    ser = serial.Serial(port, baud_rate,
-                        bytesize=serial.EIGHTBITS,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        timeout=timeouts)
-    
-    print(f"UART {port} opened! bps {baud_rate}.")
-    print("Start Reading, input 'CTRL-C' to exit...")
+ser = serial.Serial('/dev/ttyMH2')
+ser.baudrate = 9600
 
-    data = ""
+print("UART {} Opened in 9600 bps".format(ser.name))
+data = ""
+while True:
     try:
-        while True:
-            data = ser.readline()
-            print(data)
-            ser.write(data.encode('utf-8'))
-            data = ""
+        data = ser.readline()
+        print (data)
+        ser.write(data)
+        data = ""
     except KeyboardInterrupt:
-        print("Interrupted by user！")
+        print ("Interrupted by user! ")
     finally:
         ser.close()
-        print("UART {} closed!".format(port))
-        
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        port_name = "/dev/ttyMH2"
-        print("Using UART {} as default!".format(port_name))
-    else:
-        port_name = sys.argv[1]
-    baud_rate = 9600
-    serial_read(port_name, baud_rate, None)
+        print ("UART {} closed".format(ser.name))
